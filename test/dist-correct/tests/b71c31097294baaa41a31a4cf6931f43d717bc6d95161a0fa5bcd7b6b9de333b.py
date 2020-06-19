8464b940bda66ecb9f113684f23464b32c758fa3
Chris Pyles <cpyles@berkeley.edu>
2020-06-19 16:44:26 -0700

    dist-correct

---
+++ test/dist-correct/tests/b71c31097294baaa41a31a4cf6931f43d717bc6d95161a0fa5bcd7b6b9de333b.py
@@ -0,0 +1,1 @@
+test = {'name': 'b71c31097294baaa41a31a4cf6931f43d717bc6d95161a0fa5bcd7b6b9de333b', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ test/dist-correct/tests/d08b1c577d0de761c307d89c9bc97626efb0e88723775acfe6f47df304345f58.py
@@ -0,0 +1,1 @@
+test = {'name': 'd08b1c577d0de761c307d89c9bc97626efb0e88723775acfe6f47df304345f58', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
