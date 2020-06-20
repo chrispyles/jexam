dfbaf07b6e1b9e5c73db967ba07c9a63f7b1eae3
Chris Pyles <cpyles@berkeley.edu>
2020-06-20 12:17:27 -0700

    added stdout check

--- test/test_jexam.py
+++ test/test_jexam.py
@@ -4,9 +4,12 @@
 
 import unittest
 import os
+import io
 import shutil
 import pathlib
 
+from contextlib import redirect_stdout
+from textwrap import dedent
 from jexam.parser import main as jexam
 
 bin_globals = {}
@@ -17,6 +20,11 @@ TEST_FILES_PATH = pathlib.Path("test")
 PARSER = bin_globals["parser"]
 
 class TestJexam(unittest.TestCase):
+
+    expected_stdout = dedent("""\
+    Generating exam 50
+    Generating exam 100
+    """)
 
     def assertFilesEqual(self, p1, p2):
         with open(p1) as f1:
@@ -49,27 +57,45 @@ class TestJexam(unittest.TestCase):
                 if os.path.split(d1)[1] == "tests":
                     continue
                 d11, d22 = os.path.join(d1, d11), os.path.join(d2, d22)
-                self.assertEqual(os.listdir(d11), os.listdir(d22), f"Contents of {d11} not correct")
+                
+                empty_tests = "tests" in os.listdir(d11) and len(os.listdir(os.path.join(d11, "tests"))) == 0
+
+                if not empty_tests:
+                    self.assertEqual(os.listdir(d11), os.listdir(d22), f"Contents of {d11} not correct")
+                else:
+                    d11_contents, d22_contents = os.listdir(d11), os.listdir(d22)
+                    d11_contents.remove("tests")
+                    try:
+                        d22_contents.remove("tests")
+                    except ValueError:
+                        pass
+                    self.assertEqual(d11_contents, d22_contents, f"Contents of {d11} not correct")
 
                 # check file contents
                 for f1111, f2222 in zip(os.listdir(d11), os.listdir(d22)):
                     f1111, f2222 = os.path.join(d11, f1111), os.path.join(d22, f2222)
                     
                     # if tests dir, look at all files in the directory
-                    if os.path.split(f1111)[1] == "tests":
+                    if os.path.split(f1111)[1] == "tests" and not empty_tests:
                         for f11111, f22222 in zip(os.listdir(f1111), os.listdir(f2222)):
                             f11111, f22222 = os.path.join(f1111, f11111), os.path.join(f2222, f22222)
                             self.assertFilesEqual(f11111, f22222)
                     
                     # otherwise, check contents
-                    else:
+                    elif not empty_tests:
                         self.assertFilesEqual(f1111, f2222)
 
     def test_notebook_seed(self):
-        self.run_and_check_jexam()
+        stdout = io.StringIO()
+        with redirect_stdout(stdout):
+            self.run_and_check_jexam()
+        self.assertEqual(stdout.getvalue().strip(), type(self).expected_stdout.strip(), "Process stdout incorrect")
     
     def test_seed_150(self):
-        self.run_and_check_jexam(150)
+        stdout = io.StringIO()
+        with redirect_stdout(stdout):
+            self.run_and_check_jexam()
+        self.assertEqual(stdout.getvalue().strip(), type(self).expected_stdout.strip(), "Process stdout incorrect")
     
     def tearDown(self):
         if os.path.exists("dist"):
