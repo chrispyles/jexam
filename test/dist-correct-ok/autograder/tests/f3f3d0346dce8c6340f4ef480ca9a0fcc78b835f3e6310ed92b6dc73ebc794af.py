fac484472e13bcd07a9e85b1b49bac040fd03888
Chris Pyles <cpyles@berkeley.edu>
2020-06-20 17:48:12 -0700

    added autograder stuff

---
+++ test/dist-correct-ok/autograder/tests/f3f3d0346dce8c6340f4ef480ca9a0fcc78b835f3e6310ed92b6dc73ebc794af.py
@@ -0,0 +1,1 @@
+test = {'name': 'f3f3d0346dce8c6340f4ef480ca9a0fcc78b835f3e6310ed92b6dc73ebc794af', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
